75330a68fdcea1427effd34b3880b50cdbd89f72
joksa19 <nikolajoksimovic95@gmail.com>
2022-08-08 16:04:57 +0200

    gallery added..

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-$('.navbar-brand').on('click', function(e){
-    e.preventDefault();
-});
-$(".nav-link").on("click", function(e){
-    e.preventDefault();
-    // console.log(this.href.slice(this.href.indexOf('#') + 1));
-    const id = this.href.slice(this.href.indexOf('#') + 1);
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $(`#${id}`).offset().top
-    },100);
-});
+$(window).ready(function(e){
+
+    // navigation links //
+    $('.navbar-brand').on('click', function(e){
+        e.preventDefault();
+    });
+
+    $(".nav-link").on("click", function(e){
+        e.preventDefault();
+        const id = this.href.slice(this.href.indexOf('#') + 1);
+        $(document.documentElement).scrollTop($(`#${id}`).offset().top);
+    });
+
+    // gallery links //
+    $('.img-gallery-link').on('click', function(e){
+        e.preventDefault();
+        // console.log(e.target.getAttribute('src'));
+        $('#modal-image').attr('src', e.target.getAttribute('src'));
+        $('#gallery-modal').modal()
+    });
+})
